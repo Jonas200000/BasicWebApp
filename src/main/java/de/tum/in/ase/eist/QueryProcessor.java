376c09dda5c331c0eb6de1f86999ae5289689497
Jonas <jonas.kallenbach@tum.de>
2022-07-13 11:35:25 +0200

    largest value

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,25 +6,38 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "Jonas";
-        }else if (query.contains("plus")){
+        } else if (query.contains("plus")) {
             String[] params = query.split(" ");
             int sum = 0;
-            for (String s : params){
+            for (String s : params) {
                 try {
                     int n = Integer.parseInt(s);
                     sum += n;
-                } catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
 
                 }
             }
             return String.valueOf(sum);
+        } else if (query.contains("largest")) {
+            String[] params = query.split(" ");
+            int largest = Integer.MIN_VALUE;
+            for (String s : params) {
+                try {
+                    if (Integer.parseInt(s) > largest) {
+                        largest = Integer.parseInt(s);
+                    }
+                } catch (NumberFormatException e) {
+
+                }
+            }
+            return String.valueOf(largest);
         } else { // TODO extend the programm here
             return "";
         }
